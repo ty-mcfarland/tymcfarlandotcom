b828b19769a348e0408d50fd19425ce377c0bdd0
bigweezyproject <bigweezyproject@gmail.com>
2020-04-21 16:45:56 -0400

    ty/bring-back

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,20 @@
 import * as firebase from 'firebase/app'
 import "firebase/analytics";
-
+import { HeaderComponent } from './components/Header'
 // Add the Firebase products that you want to use
 import "firebase/auth";
-import { HeaderComponent } from './../node_modules/tmc-component-library/src'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-
-class AppComponent extends React.Component {
+import './styles.scss'
+class Resume extends React.Component {
     constructor(props: any) {
         super(props)
     }
     public render() {
         return(
-            <HeaderComponent />
+           <HeaderComponent />
         )
     }
 }
 
-ReactDOM.render(<AppComponent />, document.getElementById('root'))
+ReactDOM.render(<Resume />, document.getElementById('root'))
